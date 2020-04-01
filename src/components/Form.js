e1f8b3827553cc990f85e43a95fefd7dfbdff5b1
vienpham2019 <vienpham2019@gmail.com>
2020-03-31 21:31:48 -0500

    Done.

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,7 +3,8 @@ import React from 'react';
 class Form extends React.Component {
   state = {
     firstName: "John",
-    lastName: "Henry"
+    lastName: "Henry",
+    submittedData: []
   }
 
   handleFirstNameChange = event => {
@@ -18,12 +19,32 @@ class Form extends React.Component {
     })
   }
 
+  handleSubmit = event => {
+    event.preventDefault()
+    // let formData = {firstName: this.state.firstName, lastName: this.state.lastName}
+    // this.setState({submittedData: [...this.state.submittedData,formData]})
+    const firstName = event.target[0].value
+    const lastName = event.target[1].value
+
+    this.sendFormDataSomeWhere({firstName,lastName})
+  }
+
+  listOfSubmissions = () => {
+    return this.state.submittedData.map(data => {
+      return <div><span>{data.firstName}</span> <span>{data.lastName}</span></div>
+    })
+  }
+
   render() {
     return (
-      <form>
-        <input type="text" name="firstName" onChange={event => this.handleFirstNameChange(event)} value={this.state.firstName} />
-        <input type="text" name="lastName" onChange={event => this.handleLastNameChange(event)} value={this.state.lastName} />
-      </form>
+      <div>
+        <form onSubmit={event => this.handleSubmit(event)}>
+          <input type="text" name="firstName" onChange={event => this.handleFirstNameChange(event)} value={this.state.firstName} />
+          <input type="text" name="lastName" onChange={event => this.handleLastNameChange(event)} value={this.state.lastName} />
+          <button type="submit">Submit</button>
+        </form>
+        {this.listOfSubmissions()}
+      </div>
     )
   }
 }
